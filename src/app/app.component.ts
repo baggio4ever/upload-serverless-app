410a6c637d354d71c4775dc16ee5419648ca3149
baggio4ever <baggio4ever@gmail.com>
2018-02-08 15:22:51 +0900

    Pattern 面白いね

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,8 @@ export class AppComponent implements AfterViewInit, DoCheck {
   rectColor = '#FF0000';
   isMouseDown = false;
 
+  myPattern = null;
+
   context: CanvasRenderingContext2D;
   animContext: CanvasRenderingContext2D;
 
@@ -45,6 +47,12 @@ export class AppComponent implements AfterViewInit, DoCheck {
     this.animCanvasWidth = animCanvas.width;
     this.animCanvasHeight = animCanvas.height;
 
+    const img = new Image();
+    img.src = '../assets/images/images.jpg';
+    img.onload = () => {
+      this.myPattern = this.context.createPattern(img,'repeat');
+    }
+
     this.startAnim();
   }
 
@@ -53,7 +61,7 @@ export class AppComponent implements AfterViewInit, DoCheck {
   }
 
   draw() {
-    const CANVAS_WIDTH = 400;
+    const CANVAS_WIDTH = 600;
     const CANVAS_HEIGHT = 400;
 
     const ctx = this.context;
@@ -64,7 +72,11 @@ export class AppComponent implements AfterViewInit, DoCheck {
 //      ctx.fillRect( 0, 0, this.rectW, this.rectH );
       ctx.fillRect( (CANVAS_WIDTH - this.rectW) / 2, (CANVAS_HEIGHT - this.rectH) / 2, this.rectW, this.rectH ); // センター基準
 
+      ctx.fillStyle = this.myPattern;
+      ctx.fillRect( 10,10,CANVAS_WIDTH-20,CANVAS_HEIGHT-20);
+
       // 四角形（マウス操作に追従する奴）
+      ctx.fillStyle = this.rectColor;
       const W = 10;
       const H = 10;
       ctx.fillRect(this.x - W / 2, this.y - H / 2, W, H);
@@ -96,7 +108,60 @@ export class AppComponent implements AfterViewInit, DoCheck {
       ctx.moveTo(95, 65);
       ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // 右目
       ctx.stroke();
+
+      // 二次曲線の例　吹き出し
+      const offsetY = 200;
+      ctx.beginPath();
+      ctx.moveTo(75, 25 + offsetY);
+      ctx.quadraticCurveTo(25, 25 + offsetY, 25, 62.5 + offsetY);
+      ctx.quadraticCurveTo(25, 100 + offsetY, 50, 100 + offsetY);
+      ctx.quadraticCurveTo(50, 120 + offsetY, 30, 125 + offsetY);
+      ctx.quadraticCurveTo(60, 120 + offsetY, 65, 100 + offsetY);
+      ctx.quadraticCurveTo(125, 100 + offsetY, 125, 62.5 + offsetY);
+      ctx.quadraticCurveTo(125, 25 + offsetY, 75, 25 + offsetY);
+      ctx.stroke();
+
+      // 三次ベジェ曲線の例　ハート
+      const offsetX = 200;
+      ctx.beginPath();
+      ctx.moveTo(75 + offsetX, 40);
+      ctx.bezierCurveTo(75 + offsetX, 37, 70 + offsetX, 25, 50 + offsetX, 25);
+      ctx.bezierCurveTo(20 + offsetX, 25, 20 + offsetX, 62.5, 20 + offsetX, 62.5);
+      ctx.bezierCurveTo(20 + offsetX, 80, 40 + offsetX, 102, 75 + offsetX, 120);
+      ctx.bezierCurveTo(110 + offsetX, 102, 130 + offsetX, 80, 130 + offsetX, 62.5);
+      ctx.bezierCurveTo(130 + offsetX, 62.5, 130 + offsetX, 25, 100 + offsetX, 25);
+      ctx.bezierCurveTo(85 + offsetX, 25, 75 + offsetX, 37, 75 + offsetX, 40);
+      ctx.fill();
+
+      // 半透明サンプル globalAlpha
+      // draw background
+      const offsetX2 = 400;
+      ctx.fillStyle = '#FD0';
+      ctx.fillRect(0 + offsetX2, 0, 75, 75);
+      ctx.fillStyle = '#6C0';
+      ctx.fillRect(75 + offsetX2, 0, 75, 75);
+      ctx.fillStyle = '#09F';
+      ctx.fillRect(0 + offsetX2, 75, 75, 75);
+      ctx.fillStyle = '#F30';
+      ctx.fillRect(75 + offsetX2, 75, 75, 75);
+      ctx.fillStyle = '#FFF';
+
+      // set transparency value
+      ctx.globalAlpha = 0.2;
+
+      // Draw semi transparent circles
+      for (let i = 0; i < 7; i++){
+        ctx.beginPath();
+        ctx.arc(75 + offsetX2, 75, 10 + 10 * i, 0, Math.PI * 2, true);
+        ctx.fill();
+      }
+      ctx.globalAlpha = 1.0;
     }
+  }
+
+  // 度からラジアンに変換
+  getRadianFromDegree( degree: number ): number {
+    return (Math.PI / 180) * degree;
   }
 
   moveW(v): void {
