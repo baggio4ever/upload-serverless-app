039a2a9e8cd0630f5aa582c6148dadec14bc4dbf
baggio4ever <baggio4ever@gmail.com>
2018-02-09 11:59:13 +0900

    サンプルを理解するために補足コメント追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -96,37 +96,37 @@ export class AppComponent implements AfterViewInit, DoCheck {
 */
     const ctx = this.animEarthContext;
 
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0,0,300,300); // clear canvas
+    ctx.globalCompositeOperation = 'destination-over'; // destination-over: 新たな図形は、描画先 Canvas の内容の背後に描かれます。
+    ctx.clearRect(0, 0, 300, 300); // clear canvas
 
     ctx.fillStyle = 'rgba(0,0,0,0.4)';
     ctx.strokeStyle = 'rgba(0,153,255,0.4)';
     ctx.save();
-    ctx.translate(150,150);
+    ctx.translate(150, 150);  // Canvas_sun.png は、300x300の大きさ
 
     // Earth
     const time = new Date();
-    ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
-    ctx.translate(105,0);
-    ctx.fillRect(0,-12,50,24); // Shadow
-    ctx.drawImage(this.earth,-12,-12);
+    ctx.rotate( ((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds() );
+    ctx.translate(105, 0);
+    ctx.fillRect(0, -12, 50, 24); // Shadow （これのおかげで場所によって月が暗くなるみたい）
+    ctx.drawImage(this.earth, -12, -12); // Canvas_earth.png は、24x24の大きさ
 
     // Moon
     ctx.save();
-    ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
-    ctx.translate(0,28.5);
-    ctx.drawImage(this.moon,-3.5,-3.5);
+    ctx.rotate( ((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds() );
+    ctx.translate(0, 28.5);
+    ctx.drawImage(this.moon, -3.5, -3.5); // Canvas_moon.png は、7x7の大きさ
     ctx.restore();
 
     ctx.restore();
 
     ctx.beginPath();
-    ctx.arc(150,150,105,0,Math.PI*2,false); // Earth orbit
+    ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
     ctx.stroke();
 
-    ctx.drawImage(this.sun,0,0,300,300);
+    ctx.drawImage(this.sun, 0, 0, 300, 300);
 
-    window.requestAnimationFrame( ()=> {this.drawEarth();});
+    window.requestAnimationFrame( () => { this.drawEarth(); } );
   }
 
   ngDoCheck() {
