abc97aa03641f60dc13b6b0c714c8a5156bdb926
baggio4ever <baggio4ever@gmail.com>
2018-02-09 11:45:34 +0900

    'An animated solar system' サンプルを試してみた。スゲー

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,11 @@ export class AppComponent implements AfterViewInit, DoCheck {
 
   context: CanvasRenderingContext2D;
   animContext: CanvasRenderingContext2D;
+  animEarthContext: CanvasRenderingContext2D;
 
   @ViewChild('myCanvas') myCanvas;
   @ViewChild('animCanvas') animCanvas;
+  @ViewChild('animEarth') animEarth;
 
   fileToUpload: File = null;
 
@@ -36,15 +38,21 @@ export class AppComponent implements AfterViewInit, DoCheck {
 
   ball = null;
 
+  sun = new Image();
+  earth = new Image();
+  moon = new Image();
+
   constructor( private fileUploadService: FileUploadService ) {
   }
 
   ngAfterViewInit() {
+    // myCanvas
     const canvas = this.myCanvas.nativeElement;
     this.context = canvas.getContext('2d');
 
     this.draw();
 
+    // animCanvas
     const animCanvas = this.animCanvas.nativeElement;
     this.animContext = animCanvas.getContext('2d');
     this.animCanvasWidth = animCanvas.width;
@@ -63,6 +71,62 @@ export class AppComponent implements AfterViewInit, DoCheck {
     };
 
     this.startAnim();
+
+
+    // animEarth
+    const animEarthCanvas = this.animEarth.nativeElement;
+    this.animEarthContext = animEarthCanvas.getContext('2d');
+
+    this.sun.src = '../assets/images/Canvas_sun.png';
+    this.earth.src = '../assets/images/Canvas_earth.png';
+    this.moon.src = '../assets/images/Canvas_moon.png';
+
+    requestAnimationFrame( ()=>{
+      console.log('ooooh');
+      this.drawEarth();
+    });
+  }
+
+  drawEarth() {
+/*    if ( this.animEarthContext == null ) {
+          console.log('this:'+this);
+          window.requestAnimationFrame( ()=> {this.drawEarth();});
+           return; 
+    }
+*/
+    const ctx = this.animEarthContext;
+
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.clearRect(0,0,300,300); // clear canvas
+
+    ctx.fillStyle = 'rgba(0,0,0,0.4)';
+    ctx.strokeStyle = 'rgba(0,153,255,0.4)';
+    ctx.save();
+    ctx.translate(150,150);
+
+    // Earth
+    const time = new Date();
+    ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
+    ctx.translate(105,0);
+    ctx.fillRect(0,-12,50,24); // Shadow
+    ctx.drawImage(this.earth,-12,-12);
+
+    // Moon
+    ctx.save();
+    ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
+    ctx.translate(0,28.5);
+    ctx.drawImage(this.moon,-3.5,-3.5);
+    ctx.restore();
+
+    ctx.restore();
+
+    ctx.beginPath();
+    ctx.arc(150,150,105,0,Math.PI*2,false); // Earth orbit
+    ctx.stroke();
+
+    ctx.drawImage(this.sun,0,0,300,300);
+
+    window.requestAnimationFrame( ()=> {this.drawEarth();});
   }
 
   ngDoCheck() {
